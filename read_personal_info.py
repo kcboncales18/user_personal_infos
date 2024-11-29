0973b85468e2e796ac2fc149332ad080584da155
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-11-29 23:03:47 +0800

    Message

--- read_personal_info.py
+++ read_personal_info.py
@@ -3,3 +3,5 @@ with open("./user_data.txt", "r") as file:
     user_infos = file.readlines()
 for lines in user_infos:
     print(lines)
+
+#I apologize, this is what I can do for now. I'm still having difficulties in doing the program 2. 
