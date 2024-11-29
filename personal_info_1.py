41d5a53776d0be69d1485a3ce4f3d4a2ad88861d
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-11-29 10:37:01 +0800

    user's age

--- personal_info_1.py
+++ personal_info_1.py
@@ -1,9 +1,14 @@
 personal_info = []
 
-def user_name ():
+def user_name ():     #user's full name
     first_name = input("What is your first name? ")
-    middle_name = input("What is your middle name?")
+    middle_name = input("What is your middle name? ")
     surname = input("what is your surname? ")
     fullname = first_name + middle_name + surname
     print("User's name is: ", fullname)
 user_name ()
+
+def user_age ():     #user's age 
+    age = int(input("Ok, how old are you now? "))
+    print(f"User's age is: {age} years old. ")
+user_age()
