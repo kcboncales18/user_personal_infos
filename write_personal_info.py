f758f2daaae3cd1460581477d1c8406302cabfa2
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-11-29 22:28:06 +0800

    Putting an empty line in every entry.

--- write_personal_info.py
+++ write_personal_info.py
@@ -5,24 +5,28 @@ with open("./user_data.txt", "a") as file:
         first_name = input("What is your first name? ")
         middle_name = input("What is your middle name? ")
         surname = input("what is your surname? ")
-        file.write(f"User's name is: {first_name} {middle_name}. {surname}\n")
+        file.write(f"\nUser's name is: {first_name} {middle_name} {surname}")
+
     user_name ()
 
     def user_age():     #user's ages 
         age = int(input("Ok, how old are you now? "))
-        file.write(f"User's age is: {age} years old.\n")
+        file.write(f"\nUser's age is: {age} years old.")
+
     user_age()
 
     def user_birth():   #user's birthday
         month = input("What month are you born? ")
         date = int(input("What is the date of your birthday? "))
         year = int(input("and what year are you born? "))
-        file.write(f"User's birthday is on: {month}, {date} {year}\n")
+        file.write(f"\nUser's birthday is on: {month}, {date} {year}")
+
     user_birth()
 
     def user_gender():  #user's sex
         sex = input("Are you a male or female? ")
-        file.write(f"\n User's sex is:{sex}\n")
+        file.write(f"\nUser's sex is:{sex}")        
+
     user_gender()
 
     def user_address(): #user's address
@@ -30,17 +34,20 @@ with open("./user_data.txt", "a") as file:
         municipality = input("What municipality your place in? ")
         province = input("What province your place in? ")
         country = input("What country do you live? ")
-        file.write(f"User lived at: {barangay}, {municipality}, {province}, {country}\n")
+        file.write(f"\nUser lived at: {barangay}, {municipality}, {province}, {country}")     
+
     user_address()
 
     def user_nationality(): #user's nationality 
         citizenship = input("What is your nationality: ")
-        file.write(f"User's nationality is:{citizenship}\n")
+        file.write(f"\nUser's nationality is:{citizenship}")
+
     user_nationality()
 
     def user_religion(): #religion of the user
         religion = input("What religion do you belong to? ")
-        file.write(f"User's religion is: {religion}\n")
+        file.write(f"\nUser's religion is: {religion}")   
+
     user_religion()
 
     while True:         #the program continues to ask         
