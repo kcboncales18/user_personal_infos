61696c8003c96d24ccbfb04dd8bc3c97c2c0ca85
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-11-29 10:44:03 +0800

    user's birthday

--- personal_info_1.py
+++ personal_info_1.py
@@ -4,11 +4,17 @@ def user_name ():     #user's full name
     first_name = input("What is your first name? ")
     middle_name = input("What is your middle name? ")
     surname = input("what is your surname? ")
-    fullname = first_name + middle_name + surname
-    print("User's name is: ", fullname)
+    print(f"User's name is: {first_name} {middle_name}. {surname}")
 user_name ()
 
 def user_age ():     #user's age 
     age = int(input("Ok, how old are you now? "))
     print(f"User's age is: {age} years old. ")
 user_age()
+
+def user_birth ():   #user's birthday
+    month = input("What month are you born? ")
+    date = int(input("What is the date of your birthday? "))
+    year = int(input("and what year are you born? "))
+    print(f"User's birthday is on: {month}, {date} {year}")
+user_birth()
