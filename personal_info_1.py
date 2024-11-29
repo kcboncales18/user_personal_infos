60fad2f9094acab7b6656b97c07c46b8fe92f0eb
kcboncales.18 <kirstencaryl.l.boncales@gmail.com>
2024-11-29 12:16:00 +0800

    user's address

--- personal_info_1.py
+++ personal_info_1.py
@@ -1,28 +1,35 @@
 personal_info = []
 
-def user_name ():     #user's full name
+def user_name():     #user's full name
     first_name = input("What is your first name? ")
     middle_name = input("What is your middle name? ")
     surname = input("what is your surname? ")
     print(f"User's name is: {first_name} {middle_name}. {surname}")
 user_name ()
 
-def user_age ():     #user's age 
+def user_age():     #user's age 
     age = int(input("Ok, how old are you now? "))
     print(f"User's age is: {age} years old. ")
 user_age()
 
-def user_birth ():   #user's birthday
+def user_birth():   #user's birthday
     month = input("What month are you born? ")
     date = int(input("What is the date of your birthday? "))
     year = int(input("and what year are you born? "))
     print(f"User's birthday is on: {month}, {date} {year}")
 user_birth()
 
-def user_gender ():  #user's sex
+def user_gender():  #user's sex
     sex = input("Are you a male or female? ")
     print("User's sex is: ", sex)
 user_gender()
+
+def user_address():
+    barangay = input("What barangay your place in? ")
+    municipality = input("What municipality your place in? ")
+    province = input("What province your place in? ")
+    print(f"User lived at: {barangay}, {municipality}, {province}")
+user_address()
 
 def user_nationality(): #user's nationality 
     citizenship = input("What is your nationality: ")
@@ -39,13 +46,15 @@ while True:         #the program continues to ask
     age = user_age()
     birthdate = user_birth()
     sex = user_gender()
+    address = user_address()
     citizenship = user_nationality()
     religion = user_religion()
     
     personal_info.append({"user_name":full_name,
                           "user_age":age, 
                           "user_birth": birthdate, 
-                          "user_gender":sex, 
+                          "user_gender":sex,
+                          "user_address": address,
                           "user_nationality":citizenship,
                           "user_religion": religion})
     
